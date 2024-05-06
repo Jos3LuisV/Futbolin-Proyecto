f7998af84ca8e5cbd965195a3f5228990f62c5c7
Jos3LuisV <149460116+Jos3LuisV@users.noreply.github.com>
2024-05-06 05:30:46 -0600

    Código leds y buzzer

--- codigoRaspberry.py
+++ codigoRaspberry.py
@@ -39,3 +39,76 @@ paleta5.direction = digitalio.Direction.INPUT
 
 paleta6 = digitalio.DigitalInOut(board.GP7)
 paleta6.direction = digitalio.Direction.INPUT
+
+#LEDS de los jugadores
+
+led = digitalio.DigitalInOut(board.GP27)
+led.direction = digitalio.Direction.OUTPUT
+
+led2 = digitalio.DigitalInOut(board.GP26)
+led2.direction = digitalio.Direction.OUTPUT
+
+#led de la porteria
+
+led17 = digitalio.DigitalInOut(board.GP0)
+led17.direction = digitalio.Direction.OUTPUT
+
+led16 = digitalio.DigitalInOut(board.GP1)
+led16.direction = digitalio.Direction.OUTPUT
+
+led15 = digitalio.DigitalInOut(board.GP2)
+led15.direction = digitalio.Direction.OUTPUT
+
+led14 = digitalio.DigitalInOut(board.GP3)
+led14.direction = digitalio.Direction.OUTPUT
+
+led13 = digitalio.DigitalInOut(board.GP19)
+led13.direction = digitalio.Direction.OUTPUT
+
+led12 = digitalio.DigitalInOut(board.GP18)
+led12.direction = digitalio.Direction.OUTPUT
+
+# Potenciometro y su botón
+
+pot = analogio.AnalogIn(board.GP28)
+min_value = 0
+max_value = 65535
+
+boton = digitalio.DigitalInOut(board.GP5)
+boton.direction = digitalio.Direction.INPUT
+
+#lee el valor del potenciometro
+def leer_potenciometro():
+    return pot.value
+
+# define un valor dependiendo de la lectura
+def mapear_valor(valor):
+    if valor <(max_value / 3) and boton.value == True:
+        return 1
+    if valor < ((max_value /3 )*2) and boton.value == True:
+        return 2
+    if valor > ((max_value /3 )*2) and boton.value == True:
+        return 3
+
+#buzzer
+
+# Configurar el zumbador (buzzer)
+buzzer = board.GP4
+
+# Tocar la canción
+def haaland():
+    # Definir las frecuencias y duraciones para simular un aplauso
+    aplauso_frecuencias = [400, 500, 600, 700, 800, 900, 1000]  # Frecuencias del aplauso
+    aplauso_duraciones = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]  # Duraciones del aplauso
+    
+    # Reproducir el aplauso
+    for freq, dur in zip(aplauso_frecuencias, aplauso_duraciones):
+        simpleio.tone(buzzer, freq, duration=dur) #sonido
+        time.sleep(0.05)  # Esperar un breve período entre cada sonido
+        
+        
+def pitido():
+    frecuencia_pitido = 3000  # Frecuencia del pitido en Hz
+    duracion_pitido = 1    # Duración del pitido en segundos
+    simpleio.tone(buzzer, frecuencia_pitido, duration=duracion_pitido) #sonido
+
